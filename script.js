329a357873990a8e59c3c3e9ad499f3681fbd15f
James Stott <japstott@gmail.com>
2024-01-09 16:56:31 +0000

    include a function to shuffle an array (sourced from third party credited in README)

--- script.js
+++ script.js
@@ -177,8 +177,25 @@ function generatePassword() {
     var characterType = getRandom(passwordOptions.characterTypesSelected);
     passwordCharacters.push(getCharacterFromType(characterType));
   }
+}
 
-  console.log(passwordCharacters);
+// Function to shuffle an array
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
 
 // Function to select a character according to its corresponding plain text description
