d1d7f6fa7fd9c3059d94aae1abbe5ae4e109a3cb
James Stott <japstott@gmail.com>
2024-01-09 14:23:23 +0000

    add a call to getPasswordOptions in generatePassword function

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var passwordOptions = getPasswordOptions();
 }
 
 // Get references to the #generate element
